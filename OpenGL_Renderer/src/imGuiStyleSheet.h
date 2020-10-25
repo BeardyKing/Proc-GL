0f504379e18f6bc19b1022104b9bab60e01c016f
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2020-10-25 20:39:53 +0000

    imgui style sheet

---
+++ OpenGL_Renderer/src/imGuiStyleSheet.h
@@ -0,0 +1,20 @@
+#ifndef IMGUISTYLESHEET_H
+#define IMGUISTYLESHEET_H
+
+#include "vendor/imgui/imgui.h"
+
+
+class imGuiStyleSheet
+{
+public:
+	imGuiStyleSheet();
+
+private:
+	/// <summary>
+	/// https://www.gitmemory.com/issue/ocornut/imgui/707/512669512
+	/// </summary>
+	void Style_Raikiri();
+};
+
+#endif // ! IMGUISTYLESHEET_H
+
--- OpenGL_Renderer/src/main.cpp
+++ OpenGL_Renderer/src/main.cpp
@@ -12,6 +12,7 @@
 #include "vendor/imgui/imgui_impl_glfw.h"
 #include "vendor/imgui/imgui_impl_opengl3.h"
 
+#include "imGuiStyleSheet.h"
 
 #include "LightObject.h"
 
@@ -53,6 +54,8 @@ int main(){
 		return -1;
 	}
 
+	imGuiStyleSheet style;
+
 	//----------------------------------//
 	//				PBR					//
 	//----------------------------------//
@@ -84,6 +87,7 @@ int main(){
 	pointLights[2].position = glm::vec3(-13.0f, 0.0f, 10.0f);
 	pointLights[3].position = glm::vec3(10.0f, -10.0f, 10.0f);
 	pointLights[4].position = glm::vec3(-13.0f, 10.0f, 10.0f);
+	pointLights[5].position = glm::vec3(-13.0f, 10.0f, -10.0f);
 
 	//----------------------------------//
 	float angle = 10.0f;
@@ -95,9 +99,10 @@ int main(){
 	//----------------------------------//
 	//				LOOP				//
 	//----------------------------------//
-	bool show_demo_window = true;
-	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	
+
+
 
 	while (!glfwWindowShouldClose(gWindow)){
 		showFPS(gWindow);
@@ -156,7 +161,6 @@ int main(){
 		//----------------------------------//
 
 		int amountOfLights = sizeof(pointLights) / sizeof(pointLights[0]);
-		std::cout << "LIGHTS IN SCENE :" << amountOfLights << std::endl;
 
 		PBR_sphereShader.use();
 		PBR_sphereShader.setUniform("amountOfLights", amountOfLights);
@@ -202,56 +206,32 @@ int main(){
 		}
 
 		//----------------------------------//
-		//				END					//
+		//				IMGUI				//
 		//----------------------------------//
 
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-
-		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-		if (show_demo_window)
-			ImGui::ShowDemoWindow(&show_demo_window);
-		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 		{
-			static float f = 0.0f;
-			static int counter = 0;
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			{
+				ImGui::Begin("Hello GUI");       
 
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
-
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
-			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
-
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			ImGui::End();
+				ImGui::Text("Number of lights in scene : %d", amountOfLights);
+				ImGui::End();
+			}
+			// Rendering
+			ImGui::Render();
+			int display_w, display_h;
+			glfwGetFramebufferSize(gWindow, &display_w, &display_h);
+			glViewport(0, 0, display_w, display_h);
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window)
-		{
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
 
-		// Rendering
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(gWindow, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		//----------------------------------//
+		//				END					//
+		//----------------------------------//
 
 
 		glfwSwapBuffers(gWindow);
@@ -410,10 +390,7 @@ void Update(double elapsedTime) {
 
 	
 	
-	if(gCursorEnabled){
-		
-	}
-	else {
+	if(!gCursorEnabled){
 		double mouseX, mouseY;
 
 
